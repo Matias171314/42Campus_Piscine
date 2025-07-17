961f8dc0ecd7d73d0feb0d6b70ded4f5eeee1a8b
Matias Nicolas Vasquez Herbozo <mvasquez@c2r13s4.42madrid.com>
2025-07-17 19:01:37 +0200

    C Piscine C 02 fixed ex01 and ex09

--- C_Piscine_C_02/ex09/ft_strcapitalize.c
+++ C_Piscine_C_02/ex09/ft_strcapitalize.c
@@ -6,22 +6,36 @@
 /*   By: mvasquez <mvasquez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 15:29:32 by mvasquez          #+#    #+#             */
-/*   Updated: 2025/07/17 16:13:53 by mvasquez         ###   ########.fr       */
+/*   Updated: 2025/07/17 18:44:31 by mvasquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int	is_alpha_numeric(char c)
+{
+	return ((c >= 'A' && c <= 'Z')
+		|| (c >= 'a' && c <= 'z')
+		|| (c >= '0' && c <= '9'));
+}
 
 char	*ft_strcapitalize(char *str)
 {
 	int	i;
+	int	new_word;
 
 	i = 0;
+	new_word = 1;
 	while (str[i] != '\0')
 	{
-		if ((str[i] >= 'a' && str[i] <= 'z' )
-			&& (str[i - 1] < 'A'
-				|| (str[i - 1] > '[' && str[i - 1] < '`' )
-				|| str[i - 1] > 'z' ))
-			str[i] -= 32;
+		if (is_alpha_numeric(str[i]))
+		{
+			if (new_word && (str[i] >= 'a' && str[i] <= 'z'))
+				str[i] -= 32;
+			else if (!new_word && (str[i] >= 'A' && str[i] <= 'Z'))
+				str[i] += 32;
+			new_word = 0;
+		}
+		else
+			new_word = 1;
 		i++;
 	}
 	return (str);
@@ -31,14 +45,15 @@ char	*ft_strcapitalize(char *str)
 
 /*int	main(void)
 {
-	char test1[] = "hola amigo";
+	char test1[] = "hOla amigo";
 	char test2[] = "123_mayuscula";
-	char test3[] = "salut, comment tu vas ?";
-    char test3[] = "42mots quarante-deux; cinquante+et+un";
-	char test4[] = "";
+	char test3[] = "salut,comment tu vas ?";
+    char test4[] = "42mots quarante-deux; cinquante+et+un";
+	char test5[] = "";
 	printf("Test 1: %s\n", ft_strcapitalize(test1));
 	printf("Test 2: %s\n", ft_strcapitalize(test2));
 	printf("Test 3: %s\n", ft_strcapitalize(test3));
 	printf("Test 4: %s\n", ft_strcapitalize(test4));
+	printf("Test 5: %s\n", ft_strcapitalize(test5));
 	return (0);
 }*/
--- C_Piscine_C_02/ex10/ft_strlcpy.c
+++ C_Piscine_C_02/ex10/ft_strlcpy.c
@@ -1,40 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mvasquez <mvasquez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/17 12:55:13 by mvasquez          #+#    #+#             */
-/*   Updated: 2025/07/17 18:31:40 by mvasquez         ###   ########.fr       */
+/*   Created: 2025/07/17 18:45:19 by mvasquez          #+#    #+#             */
+/*   Updated: 2025/07/17 18:59:06 by mvasquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strncpy(char *dest, char *src, unsigned int n)
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
-	unsigned int	i;
-
-	i = 0;
-	while ((i < n) && src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	while (i < n)
-	{
-		dest[i] = '\0';
-		i++;
-	}
-	return (dest);
-}
-
-#include <stdio.h>
-
-int	main (void)
-{
-	char src[] = "prueba123";
-	char dest[] = "";
-	printf("dest antes: %s\n", dest);
-	printf("dest despues: %s", ft_strncpy(dest, src, 10));
-	return (0);
-}
+    unsigned int	i;
